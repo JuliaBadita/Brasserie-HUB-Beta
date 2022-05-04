ed2144843ffdfc586e9fb839e36ee6f56d64e0dc
julia <juliabadita@live.fr>
2022-05-04 17:47:19 +0200

    adding the other yellow button

---
+++ src/components/ButtonShop.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ButtonYellow.css'
+
+const ButtonShop = () => {
+  return (
+    <div className="container__button-yellow">
+      <button className="button-yellow">Notre Shop</button>
+    </div>
+  )
+}
+
+export default ButtonShop
